54ba8b766a44020930e14be7c099460663e64d20
135ahmedsayed <ahmedshkra246@gmail.com>
2025-10-04 21:28:48 +0300

    Rest_APIs

---
+++ ECommerce.Web/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Web.Controllers;
+[ApiController]
+[Route("api/[Controller]")]
+public class ProductsController : ControllerBase
+{
+    [Route("{id}")] // variable segment
+    [HttpGet]
+    //[HttpGet("{id}")]
+    public ActionResult Get(int id)
+    {
+        return Ok(new products { Id = id, Name = "Product "});
+    }
+    [HttpGet]
+    public ActionResult GetAll()
+    {
+        return Ok(new products { });
+    }
+    [HttpPost]
+    public ActionResult Create(products product)
+    {
+        return Created("Test", product);
+    }
+    [HttpPut]
+    public ActionResult Update(products product)
+    {
+        return Ok( product);
+    }
+    [HttpDelete]
+    public ActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+}
+
+//type Testing 
+public class products
+{
+    public int Id { get; set; } = 1;
+    public string Name { get; set; } = "qqwdqwd";
+}
