435abba718147a52b96d3990efbee024ac5f0a6a
135ahmedsayed <ahmedshkra246@gmail.com>
2025-10-04 22:16:50 +0300

    Onion Architecture

--- ECommerce.Web/Controllers/ProductsController.cs
+++ ECommerce.Web/Controllers/ProductsController.cs
@@ -5,33 +5,40 @@ namespace ECommerce.Web.Controllers;
 [Route("api/[Controller]")]
 public class ProductsController : ControllerBase
 {
-    [Route("{id}")] // variable segment
-    [HttpGet]
-    //[HttpGet("{id}")]
-    public ActionResult Get(int id)
-    {
-        return Ok(new products { Id = id, Name = "Product "});
-    }
-    [HttpGet]
-    public ActionResult GetAll()
-    {
-        return Ok(new products { });
-    }
-    [HttpPost]
-    public ActionResult Create(products product)
-    {
-        return Created("Test", product);
-    }
-    [HttpPut]
-    public ActionResult Update(products product)
-    {
-        return Ok( product);
-    }
-    [HttpDelete]
-    public ActionResult Delete(int id)
-    {
-        return NoContent();
-    }
+    #region Restful API Endpoints
+    //[Route("{id}")] // variable segment
+    //[HttpGet]
+    ////[HttpGet("{id}")]
+    //public ActionResult Get(int id)
+    //{
+    //    return Ok(new products { Id = id, Name = "Product "});
+    //}
+
+    //[HttpGet]
+    //public ActionResult GetAll()
+    //{
+    //    return Ok(new products { });
+    //}
+
+    //[HttpPost]
+    //public ActionResult Create(products product)
+    //{
+    //    return Created("Test", product);
+    //}
+
+    //[HttpPut]
+    //public ActionResult Update(products product)
+    //{
+    //    return Ok( product);
+    //}
+
+    //[HttpDelete]
+    //public ActionResult Delete(int id)
+    //{
+    //    return NoContent();
+    //}
+    #endregion
+
 }
 
 //type Testing 
