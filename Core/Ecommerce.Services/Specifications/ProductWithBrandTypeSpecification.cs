0edd162e234c08c61a802307a47246d3ec40861e
135ahmedsayed <ahmedshkra246@gmail.com>
2025-10-18 21:44:06 +0300

    Add SearchByNameOfProduct

--- Core/Ecommerce.Services/Specifications/ProductWithBrandTypeSpecification.cs
+++ Core/Ecommerce.Services/Specifications/ProductWithBrandTypeSpecification.cs
@@ -16,7 +16,8 @@ internal class ProductWithBrandTypeSpecification : BaseSpecification<Product>
     private static Expression<Func<Product,bool>> CreateCriteira(ProductQueryParameter parameters)
     {
         return p => (!parameters.BrandId.HasValue || p.BrandId == parameters.BrandId.Value)
-                    &&(!parameters.TypeId.HasValue || p.TypeId == parameters.TypeId.Value);
+                    &&(!parameters.TypeId.HasValue || p.TypeId == parameters.TypeId.Value)
+                    &&(string.IsNullOrWhiteSpace(parameters.Search)|| p.Name.Contains(parameters.Search!));
     }
 
     // Criteria to filter by id
--- Ecommerce.Shared/DTOs/Products/ProductQueryParameter.cs
+++ Ecommerce.Shared/DTOs/Products/ProductQueryParameter.cs
@@ -3,4 +3,5 @@ public class ProductQueryParameter
 {
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
+    public string? Search { get; set; }
 }
