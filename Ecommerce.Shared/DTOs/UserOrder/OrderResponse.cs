8dbf15a8f2b7f85583eb3f30dba804f62a39975e
135ahmedsayed <ahmedshkra246@gmail.com>
2025-11-09 22:06:48 +0200

    Create OrderService

---
+++ Ecommerce.Shared/DTOs/UserOrder/OrderResponse.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Shared.DTOs.Users;
+
+namespace Ecommerce.Shared.DTOs.UserOrder;
+public class OrderResponse
+{
+    public Guid Id { get; set; }
+    public string UserEmail { get; set; } = default!;
+    public string deliveryMethod { get; set; } = default!;
+    public decimal DeliveryMethodCost { get; set; }
+    public decimal Subtotal { get; set; }
+    public decimal Total { get; set; }
+    public AddressDTO Address { get; set; } = default!;
+    public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+    public string Status { get; set; } = default!;
+    public string PaymentIntentId { get; set; } = string.Empty;
+    public ICollection<OrderItemDTO> Items { get; set; } = [];
+}
